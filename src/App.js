bd1e0b07b1659d1ee4d5af93b38d1066e924096f
balaravanan <balaravanan81726@gmail.com>
2022-08-20 11:06:17 +0530

    summa

--- src/App.js
+++ src/App.js
@@ -3,47 +3,46 @@ import "./App.css"
 
 function App() {
 
-  const [id, setId] = useState("");
-  const [name, setName] = useState("");
-  const [role, setRole] = useState("");
-  const [employee, setEmployee] = useState([]);
+  const[id, setId] = useState("");
+  const[name, setName] = useState("");
+  const[role, setRole] = useState("");
+  const[employee, setEmployee] = useState([]);
 
-  const AddEmployee = () => {
-    setEmployee([...employee,{id:id, name:name, role:role, empid:(employee.length+1)}]);
+  const addEmployee = () => {
+    setEmployee([...employee,{id:id,role:role,name:name,empId:(employee.length+1)}]);
     setId("");
     setName("");
     setRole("");
   }
 
-  const DeleteEmployee = () => {
-      setEmployee([]);
+  const deleteAll = () => {
+    setEmployee([]);
   }
 
-  const deleteEmployee = (id) => {
-    const newEmp = employee.filter(e=> e.empid !== id);
-    setEmployee(newEmp);
+  const deleteEmployye = (id) => {
+      const newEmp = employee.filter((e)=>e.empId != id);
+      setEmployee(newEmp);
   }
-
   return (
     <div>
       <fieldset>
         <legend><h3>Employee Management</h3></legend>
         <div className='form-group'>
-          <input type="number" placeholder="Enter Id" required value={id} onChange={e=>setId(e.target.value)}/> <br/>
-          <input type="text" placeholder="Enter Name" required value={name} onChange={e=>setName(e.target.value)}/> <br/>
-          <input type="text" placeholder="Enter Role" required value={role} onChange={e=>setRole(e.target.value)}/> <br/>
-          <button onClick={AddEmployee}>Add</button> <br/>
-          <button onClick={DeleteEmployee}>Delete All</button>
+          <input type="number" placeholder='Enter Id' value={id} onChange={e=>setId(e.target.value)}/> <br/>
+          <input type="text" placeholder='Enter Name' value={name} onChange={e=>setName(e.target.value)}/> <br/>
+          <input type="text" placeholder='Enter Role' value={role} onChange={e=>setRole(e.target.value)}/> <br/>
+          <button onClick={addEmployee}>Add</button> <br/>
+          <button onClick={deleteAll}>Delete All</button>
         </div>
-      </fieldset>
+      </fieldset>  
 
-    {employee.map((emp,i)=>{
-      return(
-        <div className='form-group' key={i}>
-            {emp.id}  {emp.name}  {emp.role} <button onClick={() => {deleteEmployee(emp.empid)}} className="XbuT">X</button>
+      { employee.map((emp,i)=>{
+        return (
+        <div className='form-group'>
+          {emp.id}  {emp.name}  {emp.role} <button onClick={()=>{deleteEmployye(emp.empId)}} className="XbuT">x</button>
         </div>
       )
-    })}
+      })}    
     </div>
   )
 }
